a551725e957e7bc2e45a42bcaa8f85af817f3a92
lx3211 <lx3211@gmail.com>
2023-01-07 17:31:36 +0800

    feat:successfully run

--- web.py
+++ web.py
@@ -1,6 +1,9 @@
 import joblib
 model_pretrained = joblib.load('Box_Office-LR-20221212.pkl')
 import numpy as np
+import pandas as pd
+
+data = pd.read_csv("train.csv")
 
 from flask import Flask, request, render_template
 app = Flask(__name__)
@@ -17,18 +20,131 @@ def submit():
     if request.method == 'POST':
         print("收到POST請求...")
 
-        form_data = request.form
-        print(form_data)
+        form_data = dict(request.form)
+        print(type(form_data))
+
         # 此為測試試印
         print("form_data['budget']"+form_data['budget'])
         print("form_data['original_language']"+form_data['original_language'])
         print("form_data['popularity']:"+form_data['popularity'])
 
+        # budget (text area)
+        if form_data['budget'] == "" :
+            form_data['budget']= '0' # default value
+
+        #language （下拉式選單
+        language_en = ''
+        language_zh = ''
+        language_other = ''
+        if int(form_data['original_language']) == 0:
+            language_en = 'selected'
+        elif int(form_data['original_language']) == 1:
+            language_zh = 'selected'
+        elif int(form_data['original_language']) == 2:
+            language_other = 'selected'
+        else: #default value
+            language_other = 'selected'
+            form_data['original_language']='0'
+
+
+        # popularity (text area)
+        if form_data['popularity'] == "" :
+            form_data['popularity']= '0' # default value
+
+        # runtime (text area)
+        if form_data['runtime'] == "" :
+            form_data['runtime']= str(data['runtime'].mean()) # default value
+
+        #homepage (radio)
+        has_homepage_yes = ''
+        has_homepage_no = ''
+
+        if int(form_data['has_homepage'])== 1:
+            has_homepage_yes = 'checked'
+        elif int(form_data['has_homepage'])== 0:
+            has_homepage_no = 'checked'
+
+
+        #collection (radio)
+        collection_yes = ''
+        collection_no = ''
+
+        if int(form_data['collection'])== 1:
+            collection_yes = 'checked'
+        elif int(form_data['collection'])== 0:
+            collection_no = 'checked'
+
+        # num_prod_companies (text area)
+        if form_data['num_prod_companies'] == "" :
+            form_data['num_prod_companies']= '1' # default value
+
+        # num_genres (text area)
+        if form_data['num_genres'] == "" :
+            form_data['num_genres']= '0' # default value
+        else:
+            form_data['num_genres']=str(len(set(form_data['num_genres'].split(" "))))
+
+        # num_prod_countries (text area)
+        if form_data['num_prod_countries'] == "" :
+            form_data['num_prod_countries']= '1' # default value
+
+        # cast_count (text area)
+        if form_data['cast_count'] == "" :
+            form_data['cast_count']= '1' # default value
+
+        # crew_count (text area)
+        if form_data['crew_count'] == "" :
+            form_data['crew_count']= '1' # default value
+
+        #release_day （下拉式選單
+        release_day_sun = ''
+        release_day_mon = ''
+        release_day_tue = ''
+        release_day_wed = ''
+        release_day_thr = ''
+        release_day_fri = ''
+        release_day_sat = ''
+
+        if int(form_data['release_day']) == 0:
+            release_day_sun = 'selected'
+        elif int(form_data['release_day']) == 1:
+            release_day_mon = 'selected'
+        elif int(form_data['release_day']) == 2:
+            release_day_tue = 'selected'
+        elif int(form_data['release_day']) == 3:
+            release_day_wed = 'selected'
+        elif int(form_data['release_day']) == 4:
+            release_day_thr = 'selected'
+        elif int(form_data['release_day']) == 5:
+            release_day_fri = 'selected'
+        elif int(form_data['release_day']) == 6:
+            release_day_sat = 'selected'
+        else: #default value
+            release_day_sun = 'selected'
+            form_data['release_day']='0'
+
+        # Keywords_count (text area)
+        if form_data['Keywords_count'] == "" :
+            form_data['Keywords_count']= '0' # default value
+
+        #isTaglineNA (radio)
+        isTaglineNA_yes = ''
+        isTaglineNA_no = ''
+
+        if int(form_data['isTaglineNA'])== 1:
+            isTaglineNA_yes = 'checked'
+        elif int(form_data['isTaglineNA'])== 0:
+            isTaglineNA_no = 'checked'
+
+        # spoken_count (text area)
+        if form_data['spoken_count'] == "" :
+            form_data['spoken_count']= '1' # default value
+
         result = model_pretrained.predict([[
         np.log1p(float(form_data['budget'])),
         int(form_data['original_language']),
         float(form_data['popularity']),
-        int(form_data['runtime']),
+        float(form_data['runtime']),
         int(form_data['has_homepage']),
         int(form_data['collection']),
         int(form_data['num_genres']),
@@ -47,20 +163,46 @@ def submit():
         prediction = result
 
         return render_template('form.html',
+        # budget = budget,
+        language_en = language_en,
+        language_zh = language_zh,
+        language_other = language_other,
+        # popularity = popularity,
+        # runtime = runtime,
+        has_homepage_yes = has_homepage_yes,
+        has_homepage_no = has_homepage_no,
+        collection_yes = collection_yes,
+        collection_no = collection_no,
+        # num_genres = num_genres,
+        # num_prod_companies = num_prod_companies,
+        # num_prod_countries = num_prod_countries,
+        # cast_count = cast_count,
+        # crew_count = crew_count,
+        # Keywords_count = Keywords_count,
+        # spoken_count = spoken_count,
+        release_day_sun = release_day_sun,
+        release_day_mon = release_day_mon,
+        release_day_tue = release_day_tue,
+        release_day_wed = release_day_wed,
+        release_day_thr = release_day_thr,
+        release_day_fri = release_day_fri,
+        release_day_sat = release_day_sat,
+        isTaglineNA_yes = isTaglineNA_yes,
+        isTaglineNA_no = isTaglineNA_no,
         budget = form_data['budget'],
-        original_language = form_data['original_language'],
+        # original_language = form_data['original_language'],
         popularity = form_data['popularity'],
         runtime = form_data['runtime'],
-        has_homepage = form_data['has_homepage'],
-        collection = form_data['collection'],
+        # has_homepage = form_data['has_homepage'],
+        # collection = form_data['collection'],
         num_genres = form_data['num_genres'],
         num_prod_companies = form_data['num_prod_companies'],
         num_prod_countries = form_data['num_prod_countries'],
         cast_count = form_data['cast_count'],
         crew_count = form_data['crew_count'],
-        release_day = form_data['release_day'],
+        # release_day = form_data['release_day'],
         Keywords_count = form_data['Keywords_count'],
-        isTaglineNA = form_data['isTaglineNA'],
+        # isTaglineNA = form_data['isTaglineNA'],
         spoken_count = form_data['spoken_count'],
         prediction = prediction)
 
